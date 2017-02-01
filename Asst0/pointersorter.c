8d1c929211b397a00b67584aa6f7726d80bee1b3
SaraAnn Stanway <sys41@java.cs.rutgers.edu>
2017-01-31 22:46:06 -0500

    frasisco-fied the string parser, cleaned everything up

--- Asst0/pointersorter.c
+++ Asst0/pointersorter.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 typedef struct node
 {
@@ -14,24 +15,9 @@ typedef struct HashTable
 	int numFilled;
 }hashTable;
 
-//transform all nonalpha characters to spaces so strtok can be used with a single delimiter
-char* parseString(char* string)
-{
-	//note* strlen doesn't counts up to but not including the null terminator
-	int i;
-	for (i=0; i<strlen(string); i++)
-	{
-		if (!isalpha(string[i]))
-		{
-			string[i] = ' ';
-		}
-	}
-
-	return string;
-}
 Node* makeNode(char* data)
 {
-	Node* myNode = (Node*)calloc(sizeof(Node));
+	Node* myNode = (Node*)calloc(1, sizeof(Node));
 	//mallocs and copies data into new string
 	myNode -> data = strdup(data);
 	myNode -> next = NULL;
@@ -42,27 +28,56 @@ Node* makeNode(char* data)
 Node* tokenize(char* string)
 {
 	Node* head = NULL;
-	const char delim [2] = " ";
-	char* curr;
-	for (curr = strtok(string, delim); curr!=NULL; curr = strtok(NULL, delim))
+	//current node to attach new additions
+	Node* mover = NULL;
+	int i, start, end, size;
+	for (i=0; i<strlen(string); i++)
 	{
-		//continuously add nodes to front of linked list
-		Node* temp = makeNode(curr);
-		temp->next = head;
-		head = temp;
+		//get start index of alphabetic characters
+		start = i;
+		while(!isalpha(string[i]))
+		{
+			start++;
+			i++;
+		}
+		//get end index of alphabetic characters
+		end = start+1;
+		while(isalpha(string[i]))
+		{
+			i++;
+			end++;
+		}
+		size = end-start+1;
+		//copy relevant characters over into temporary string
+		char* temp = (char*)malloc(sizeof(char)*size);
+		memcpy(temp, string+start, size);
+		//make sure temporary string is properly null terminated
+		temp[size-1] = '\0';
+		Node* curr = makeNode(temp);
+		//initialize list if necessary
+		if(head == NULL)
+			{
+				head = curr;
+				mover = head;
+			}
+		//add on to existing list otherwise
+		else
+			{
+				mover->next = curr;
+				mover = mover->next;
+			}
 	}
+	
 	return head;
 }
-
 hashTable* makeHashTable()
 {
-	hashTable* myTable = (hashTable*)calloc(sizeof(hashTable));
+	hashTable* myTable = (hashTable*)calloc(1, sizeof(hashTable));
 	//not entirely sure if this is a double pointer. i'm pretty sure it is though
-	myTable -> table = (Node**)calloc(sizeof(Node*)*52);
+	myTable -> table = (Node**)calloc(52, sizeof(Node*));
 	myTable -> numFilled = 0;
 	return myTable;
 }
-
 //works for determined edge cases. will run more scenarios
 int checkInput(int argc, char** argv)
 {
@@ -70,7 +85,7 @@ int checkInput(int argc, char** argv)
 	//too few or too many inputs
 	if(argc!=2)
 		{
-			printf("usage: pointersorter.c input_string");
+			printf("usage: pointersorter.c input_string\n");
 			return 1;
 		}
 	//string is empty or contains no alphas	
@@ -79,16 +94,14 @@ int checkInput(int argc, char** argv)
 		return 1;
 	}
 	return 0;
-}
 
-void sort(Node* head)
+}
+hashTable* sort(Node* head)
 {
 	//slot in the hashTable according to leading letter
 	int index;
 	//leading letter
 	int leading;
-	//ordering rank
-	int rank;
 	hashTable* hTable = (hashTable*)malloc(sizeof(hashTable));
 	//insert each Node into hashTable, staggering upper and lower cases for intuitive ordering
 	while(head!=NULL)
@@ -101,12 +114,12 @@ void sort(Node* head)
 		else
 			index = 2*(leading-97)-1;
 		//node to be inserted
-		Node* node = makeNode(head.data);
+		Node* node = makeNode(head->data);
 		//node already at the desired index
 		Node* curr = hTable->table[index];
 		Node* prev = hTable->table[index];
 		//if node is to be inserted at front of list
-		if(hTable[index]==NULL || strcmp(curr->data, node->data)<=0)
+		if(hTable->table[index]==NULL || strcmp(curr->data, node->data)<=0)
 			{
 				node->next = curr;
 				hTable->table[index] = node;
@@ -115,26 +128,21 @@ void sort(Node* head)
 			else
 			{
 			//while string to be inserted comes after existing strings
-				while(curr!=null && strcmp(curr->data, node->data)>0)
+				while(curr!=NULL && strcmp(curr->data, node->data)>0)
 				{	
-					else
-					{
-						prev = curr;
-						curr = curr.next;
-					}
-				
+					prev = curr;
+					curr = curr->next;
 				}
-				node.next = curr;
-				prev.next = node;
+				node->next = curr;
+				prev->next = node;
 			}
 	}
+	return hTable;
 }
-
-
 void printTable(hashTable* hTable)
 {
 	int i;
-	Node curr;
+	Node* curr;
 	for (i=0; i<52; i++)
 	{
 		curr = hTable->table[i];
@@ -149,7 +157,8 @@ void printTable(hashTable* hTable)
 void destroyTable(hashTable* hTable)
 {
 	int i;
-	Node curr, temp;
+	Node* curr;
+	Node* temp;
 	for (i=0; i<52; i++)
 	{
 		curr = hTable->table[i];
@@ -165,10 +174,10 @@ void destroyTable(hashTable* hTable)
 //free unsorted temp linked list
 void destroyList(Node* head)
 {
-	Node temp;
+	Node* temp;
 	while(head!=NULL)
 	{
-		temp = head.next;
+		temp = head->next;
 		free(head);
 		head = temp;
 	}
@@ -176,12 +185,14 @@ void destroyList(Node* head)
 int main(int argc, char** argv)
 {
 	//exit if invalid input
-	if (checkInput(arc, argv) == 1)
+	if (checkInput(argc, argv) == 1)
 		return 1;
-	char*
-	
-	
+	char* inputString = argv[1];
+	Node* stringList = tokenize(inputString);		
+	hashTable* myTable = sort(stringList);
+	printTable(myTable);
+	destroyTable(myTable);
+	destroyList(stringList);
 	return 0;
 }
-
 
