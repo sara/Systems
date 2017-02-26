6ba7b17aa1b3dcb054306a44446a6b665f124064
sara <sarastanway@gmail.com>
2017-02-26 04:16:21 -0500

    WE DID IT HURRAYgit add memgrind.cgit add memgrind.c

--- Asst1/memgrind.c
+++ Asst1/memgrind.c
@@ -18,11 +18,11 @@ void grindA()
         char * myPointers[1000];
 		initMyPointers(myPointers);
         int i = 0;
-        for(i = 0; i<1; i++)
+        for(i = 0; i<1000; i++)
 		{
                 myPointers[i] = (char*)malloc(1*sizeof(char));
         }
-        for(i = 0; i<1; i++)
+        for(i = 0; i<1000; i++)
 		{
                 free(myPointers[i]);
         }
@@ -107,73 +107,90 @@ void grindC()
 
 void grindD()
 {
-	char* myPointers [1000];
-	initMyPointers(myPointers);
-	int size, operation;
-	int index = 0;
-	int j;
-	int numMallocs = 0;
+	char* myPointers[1000];
 	int numFrees = 0;
-	int spaceUsed = 2;
-	while (numMallocs < 1000)
-	{
-		//odd indicates malloc, even indicates free
-		operation = (rand()+1) % 2;
-		//if malloc
-		if (operation == 1 && spaceUsed < 5000)
-		{
-			numMallocs ++;
-			//generate a pointer between 1 and 65 bytes
-			size = (rand() %65) + 1;
-			char* p = (char*)malloc(size*sizeof(char));
-			if (size%2 == 1)
-				size++;
-			myPointers[index] = p;
-			index ++;
-			spaceUsed += size+2;
-		}
-		else
-		{
-			j=0;
-			while (myPointers[j] == NULL)
-			{
-				j++;
-			}
-			spaceUsed -= sizeof(myPointers[j]) +2;
-			if (sizeof(myPointers[j])%2 == 1)
-				spaceUsed --;
-			myPointers[j] = NULL;
-		}
-		printf("NumMallocs: %i, space used: %i\n", numMallocs, spaceUsed); 
-	}
-	j = 0;
-	while (spaceUsed >0)
-	{
-		
-		if (myPointers[j] != NULL)
-		{
-			spaceUsed -= *myPointers[j] + 2;
-			myPointers[j] = NULL;
-			j++;
-		}
-	}
-	printf("final space used: %i\n", spaceUsed); 
-}
-//start at mallocing 5000, then decrement by two, alternating malloc and free each time
+	int numMallocs = 0;
+	int numSaved = 0;
+	int arrIndex = 0;
+	int freeIndex = 0;
+	int op, i;
+	int spaceUsed = 0;
+	int amtData = 0;
+	initMyPointers(myPointers);
 
+		while(numFrees<1000)
+		{
+			op = rand();
+			amtData = 1+rand()%64;
+			if (amtData+2+spaceUsed > 5000)
+			{
+				op = 1;
+			}
+
+			if (op%2 == 0 && numMallocs == 1000)
+				op = 1;
+			else if (op%2 == 1 && numSaved ==0)
+				op =0;
+			if (op%2==1)
+			{
+				numFrees++;
+				free(myPointers[freeIndex]);
+				freeIndex ++;
+				numSaved --;
+			}
+			else
+			{
+				char* ptr = (char*)malloc(amtData*sizeof(char));
+				if (ptr!=0)
+				{	
+					numMallocs ++;
+					myPointers[arrIndex] = ptr;
+					spaceUsed += amtData+2;
+					arrIndex++;
+					numSaved ++;
+				}
+			}
+		}
+	printf("arrIndex = %i, freeIndex = %i\n", arrIndex, freeIndex);
+}
+
+//start at mallocing 5000, then decrement by two, alternating malloc and free each time
 void grindE()
 {
-	
+	char * tmp = NULL;
+	int k = 0;
+	for(k = 0; k < 2497; k++)
+	{
+		tmp = (char*)malloc(4996-2*k);
+		free(tmp);
+	}
 }
 
+void grindF()
+{
+	char * p = NULL;
+	int i = 0;
+	for(i = 4900; i > 0; i-=100)
+	{
+		printf("F: %d\n",i);
+		p = (char*)malloc(i*sizeof(char));
+		free(p);
+	}
+}
 
 void main(int argc, char * argv[])
 {
-//	grindA();
-//	grindB();
+	grindA();
+	grindB();
 	grindC();
-//	grindD();
+	grindD();
+	grindE();
+	grindF();
 //  int x;
 //  free(&x);
+//	char * sara = (char*)malloc(4998*sizeof(char));
+	 
+	printf("\n:)\n");	
+//	free(sara);
 }
 
